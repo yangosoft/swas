2b65ee4abcf72016ba4721c5420d8d51aab85f2e
Yangosoft <yangosoft@gmail.com>
2019-01-11 15:03:15 +0100

    Seekable audio

--- server/routes/play.js
+++ server/routes/play.js
@@ -7,22 +7,49 @@ var fs = require('fs');
 router.get('/play/:id', function(req, res, next) {
   var app = require('../app');
   var s = app.get('scanner');
-  var path = s.getPathById(req.params.id);
+  var path = s.getPathById(parseInt(req.params.id),10);
   console.log("Param " + req.params.id + " path " + path);
 
   if (path === null) {
     res.end(404);
   } else {
-    var inputStream = fs.createReadStream(path);
-    inputStream.pipe(res);
+
+
+    if (req.headers.range) {
+      var stat = fs.statSync(path);
+      var total = stat.size;
+      var range = req.headers.range;
+      var parts = range.replace(/bytes=/, "").split("-");
+      var partialstart = parts[0];
+      var partialend = parts[1];
+
+      var start = parseInt(partialstart, 10);
+      var end = partialend ? parseInt(partialend, 10) : total - 1;
+      var chunksize = (end - start) + 1;
+      var readStream = fs.createReadStream(path, {
+        start: start,
+        end: end
+      });
+      res.writeHead(206, {
+        'Content-Range': 'bytes ' + start + '-' + end + '/' + total,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': 'video/mp4'
+      });
+      readStream.pipe(res);
+    } else {
+      var inputStream = fs.createReadStream(path);
+      inputStream.pipe(res);
+    }
+
   }
 });
 
 router.get('/list', function(req, res, next) {
-    var app = require('../app');
-    var s = app.get('scanner');
-    var l = s.getFileList();
-    res.json( l );
+  var app = require('../app');
+  var s = app.get('scanner');
+  var l = s.getFileList();
+  res.json(l);
 });
 
 module.exports = router;
